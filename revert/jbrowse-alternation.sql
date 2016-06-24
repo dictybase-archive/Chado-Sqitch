f37138062ad83e66a589b06c9521e60852148fca
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2016-06-24 05:58:14 -0500

    Added the correct table name to remove the constraints

--- revert/jbrowse-alternation.sql
+++ revert/jbrowse-alternation.sql
@@ -5,8 +5,8 @@ BEGIN;
 ALTER TABLE jbrowse_organism DROP CONSTRAINT jbrowse_organism_dataset;
 ALTER TABLE jbrowse_organism DROP COLUMN dataset;
 ALTER TABLE jbrowse_track ALTER COLUMN type_id SET NOT NULL; 
-ALTER TABLE jbrowse_organism DROP CONSTRAINT jbrowse_track_exist_label;
-ALTER TABLE jbrowse_organism DROP CONSTRAINT jbrowse_track_unique_label;
+ALTER TABLE jbrowse_track DROP CONSTRAINT jbrowse_track_exist_label;
+ALTER TABLE jbrowse_track DROP CONSTRAINT jbrowse_track_rest_baseurl;
 DROP INDEX jbrowse_track_unique_label;
 
 COMMIT;
