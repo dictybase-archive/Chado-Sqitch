54f417c344ebebdccdf3bbeeb5ba90d9ab8fa04f
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2016-06-24 17:06:55 -0500

    fixed all sql syntax errors

--- deploy/auth.sql
+++ deploy/auth.sql
@@ -5,13 +5,13 @@ BEGIN;
 
 create table auth_permission (
     auth_permission_id serial not null,
-    permission varchar(255) not null
+    permission varchar(255) not null,
     description text,
     primary key(auth_permission_id),
     constraint auth_permission_c1 unique(permission),
     created_at timestamp with time zone default now(),
     updated_at timestamp with time zone default now()
-)
+);
 
 COMMENT ON TABLE auth_permission IS 'Permission defines what an user can do in the system, create-user, delete-user, 
 create-gene-model etc. It defined a granular level of actions for the user';
@@ -24,26 +24,26 @@ create table auth_role (
     auth_role_id serial not null,
     role varchar(255) not null,
     description text,
-    primary key(auth_role_id)
+    primary key(auth_role_id),
     created_at timestamp with time zone default now(),
     updated_at timestamp with time zone default now()
-)
+);
 
 COMMENT ON TABLE auth_role IS 'An auth role is a set of activities. It defines a set of permission an user can perform';
 
-COMMENT ON COLUMN role IS 'The name of the role, could be arbitrary';
+COMMENT ON COLUMN auth_role.role IS 'The name of the role, could be arbitrary';
 
 create table auth_role_permission (
     auth_role_permission_id serial not null,
     auth_role_id int not null, 
     auth_permission_id int not null,
     primary key(auth_role_permission_id),
-    foreign key (auth_role_id) references auth_role(auth_role_id) on delete cascade INITIALLY DEFERRED;
-    foreign key (auth_permission_id) references auth_permission(auth_permission_id) on delete cascade INITIALLY DEFERRED;
+    foreign key (auth_role_id) references auth_role(auth_role_id) on delete cascade INITIALLY DEFERRED,
+    foreign key (auth_permission_id) references auth_permission(auth_permission_id) on delete cascade INITIALLY DEFERRED,
     created_at timestamp with time zone default now(),
     updated_at timestamp with time zone default now(),
     constraint auth_role_permission_c1 unique (auth_role_id, auth_permission_id)
-)
+);
 
 COMMENT ON TABLE auth_role_permission IS 'The linking table between auth_permission and auth_role. 
 Allows to define roles with activities and the same permission spreads accross multiple roles.';
@@ -65,7 +65,7 @@ COMMENT ON TABLE auth_user IS 'The primary table for the user information, it on
 COMMENT ON COLUMN auth_user.email IS 'Store emails of every user, the value has to be unique.
 Email is treated case-insensitive and citext provides a case-insensitive data type';
 
-COMMENT ON COLUMN auth.is_active IS 'Column for active or inactive user';
+COMMENT ON COLUMN auth_user.is_active IS 'Column for active or inactive user';
 
 create table auth_user_info (
     auth_user_info_id serial not null,
@@ -80,7 +80,7 @@ create table auth_user_info (
     phone varchar(30),
     auth_user_id int not null,
     primary key(auth_user_info_id),
-    foreign key(auth_user_id) references auth_user(auth_user_id)  on delete cascade INITIALLY DEFERRED;
+    foreign key(auth_user_id) references auth_user(auth_user_id)  on delete cascade INITIALLY DEFERRED
 );
 
 COMMENT ON TABLE auth_user_info IS 'A table to store optional user information.';
@@ -89,8 +89,8 @@ create table auth_user_role (
     auth_user_role_id serial not null,
     auth_user_id int not null,
     auth_role_id int not null,
-    foreign key (auth_user_id) references auth_user(auth_user_id) on delete cascade INITIALLY DEFERRED;
-    foreign key (auth_role_id) references auth_role(auth_role_id) on delete cascade INITIALLY DEFERRED;
+    foreign key (auth_user_id) references auth_user(auth_user_id) on delete cascade INITIALLY DEFERRED,
+    foreign key (auth_role_id) references auth_role(auth_role_id) on delete cascade INITIALLY DEFERRED
 );
 
 COMMENT ON TABLE auth_user_role IS 'Linking table between user and role';
